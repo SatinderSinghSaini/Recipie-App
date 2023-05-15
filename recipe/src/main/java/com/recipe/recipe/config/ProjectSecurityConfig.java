55625a90ee828b277d45e562a573f2edb6c920e8
Satinder Singh Saini <swv095@gmail.com>
2023-05-15 18:19:29 +0530

    Implemented Authentication using Database

--- recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
+++ recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
@@ -3,17 +3,24 @@ package com.recipe.recipe.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
+
     @Bean
-    SecurityFilterChain defaultSecurityFilter(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/recipes").authenticated()
-            .and().formLogin()
-            .and().httpBasic();
-        return (SecurityFilterChain) httpSecurity.build();
+    UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
