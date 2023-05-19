7d2c7732dcf261ac3dfbd0d207fc3baf812fd7e5
Satinder Singh Saini <swv095@gmail.com>
2023-05-19 18:30:31 +0530

    Implemented BCryptPassword

--- recipe/src/main/java/com/recipe/recipe/controllers/RegisterAppUserController.java
+++ recipe/src/main/java/com/recipe/recipe/controllers/RegisterAppUserController.java
@@ -3,6 +3,7 @@ package com.recipe.recipe.controllers;
 import com.recipe.recipe.models.AppUser;
 import com.recipe.recipe.repositories.AppUserJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,16 @@ public class RegisterAppUserController {
 
     @Autowired
     private AppUserJpaRepository appUserJpaRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @PostMapping
     public AppUser saveAppUser(@RequestBody AppUser appUser){
         try{
+            appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
             AppUser userInDb = appUserJpaRepository.save(appUser);
+            return userInDb;
         }catch(Exception e){
             throw new RuntimeException("An exception occurred while saving user"+ e.getMessage());
         }
-        return appUser;
     }
 }
--- recipe/src/main/resources/sqlScripts.sql
+++ recipe/src/main/resources/sqlScripts.sql
@@ -2,7 +2,7 @@ use recipe;
 create table users(
 id INT NOT NULL AUTO_INCREMENT,
 username VARCHAR(45) NOT NULL,
-password VARCHAR(45) NOT NULL,
+password VARCHAR(100) NOT NULL,
 enabled INT NOT NULL,
 PRIMARY KEY(id)
 );
