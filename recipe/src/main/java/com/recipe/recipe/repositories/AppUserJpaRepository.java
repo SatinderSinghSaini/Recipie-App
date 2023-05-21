0d0b2447eec0a6f8c294cab3743461778264f02b
Satinder Singh Saini <swv095@gmail.com>
2023-05-21 17:42:44 +0530

    This is path of Custom Authentication Provider implementation

--- recipe/src/main/java/com/recipe/recipe/repositories/AppUserJpaRepository.java
+++ recipe/src/main/java/com/recipe/recipe/repositories/AppUserJpaRepository.java
@@ -3,5 +3,8 @@ package com.recipe.recipe.repositories;
 import com.recipe.recipe.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppUserJpaRepository extends JpaRepository<AppUser,Long> {
+    List<AppUser> getAppUserByUserName(String userName);
 }
