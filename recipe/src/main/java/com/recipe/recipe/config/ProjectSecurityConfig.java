7b1a079fc6da1b6ee0a7212b2fb8a38c2d228d0f
Satinder Singh Saini <swv095@gmail.com>
2023-05-22 18:05:22 +0530

    CSRF changes

--- recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
+++ recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.recipe.recipe.config;
 
+import com.recipe.recipe.filters.AddCSRFCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,15 +10,23 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+        http.csrf((csrf)->csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                            .ignoringRequestMatchers("/register")
+                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                        .addFilterAfter(new AddCSRFCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/recipes/**").authenticated()
                 .requestMatchers("/register").permitAll()
