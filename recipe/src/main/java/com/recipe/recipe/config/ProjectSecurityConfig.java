cccd7e649dbb90392a8f4de2f8aa8cbe85d35e8d
Satinder Singh Saini <swv095@gmail.com>
2023-06-20 14:11:29 +0530

    Implemented CORS

--- recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
+++ recipe/src/main/java/com/recipe/recipe/config/ProjectSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.recipe.recipe.config;
 
 import com.recipe.recipe.filters.AddCSRFCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,8 +16,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -23,12 +28,28 @@ public class ProjectSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
         csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
-        http.csrf((csrf)->csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+
+        http.securityContext().requireExplicitSave(false).
+                and().sessionManagement((session)->session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfiguration.setAllowCredentials(true);
+                        corsConfiguration.setMaxAge(3600L);
+                        return corsConfiguration;
+                    }
+                }).and()
+                .csrf((csrf)->csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
                             .ignoringRequestMatchers("/register")
                             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                         .addFilterAfter(new AddCSRFCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/recipes/**").authenticated()
+                .requestMatchers("/user").authenticated()
                 .requestMatchers("/register").permitAll()
                 .and()
                 .formLogin()
@@ -37,10 +58,10 @@ public class ProjectSecurityConfig {
 
     }
 
-    @Bean
+   /* @Bean
     UserDetailsService userDetailsService(DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
 
     @Bean
     PasswordEncoder passwordEncoder(){
